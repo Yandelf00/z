c0065448cf5908e8e535516ed5cb18aa4b44c3f8
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2025-04-04 23:25:36 +0000

    idk

---
+++ src/app/(general)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+export default function Home() {
+  return (
+    <main>
+    <LoginLink>Sign in</LoginLink>
+    <RegisterLink>Sign up</RegisterLink>
+    </main>
+  );
+}
